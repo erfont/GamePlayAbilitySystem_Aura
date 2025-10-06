3c610730c266f105d94d0987b473b010d8aca595
Jose Maria Font Fernandez <jose.font@mau.se>
2025-10-06 10:38:38 +0200

    player state created

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Jose Font
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	SetNetUpdateFrequency(100.f); // How often the server will try to update changes
+}
