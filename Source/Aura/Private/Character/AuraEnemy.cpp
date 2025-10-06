ec417c59a25948dbdca0cc2dfea76794365dcd2f
Jose Maria Font Fernandez <jose.font@mau.se>
2025-10-06 10:01:37 +0200

    all enemies highlighted

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,17 +2,28 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
 	UE_LOG(LogTemp, Warning, TEXT("Highlighted"));
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
 	UE_LOG(LogTemp, Warning, TEXT("UNHighlighted"));
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
+
 
 }
